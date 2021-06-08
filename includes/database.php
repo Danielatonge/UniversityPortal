ddfaa61f33198a0547bfd6ad10cf222915a17c79
Danielatonge <elamboatonge@gmail.com>
2021-06-09 00:54:58 +0300

    updates

--- includes/database.php
+++ includes/database.php
@@ -1,11 +1,10 @@
 <?php
 
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("mysql://b2d89fe57b5978:241c8e67@us-cdbr-east-04.cleardb.com/heroku_e7ec67a7705bcd0?reconnect=true"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
+$cleardb_server = 'us-cdbr-east-04.cleardb.com';
+$cleardb_username = 'b2d89fe57b5978';
+$cleardb_password = '241c8e67';
+$cleardb_db = 'heroku_e7ec67a7705bcd0';
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
